9fbbfb21fe343b4eb34e096b2f96e626e6504d8a
QuickDeck <105922353+QuickDeck@users.noreply.github.com>
2026-01-16 12:56:24 -0500

    Set up symbolic link script

---
+++ create_symbolic_links.bat
@@ -0,0 +1,6 @@
+mklink /J "%HOMEDRIVE%\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data\Editor\Mods\Swashbuckler2e_7652d95c-1ede-6b99-e5b7-229c6a80b94f" ".\Editor\Mods\Swashbuckler2e_7652d95c-1ede-6b99-e5b7-229c6a80b94f"
+mklink /J "%HOMEDRIVE%\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data\Mods\Swashbuckler2e_7652d95c-1ede-6b99-e5b7-229c6a80b94f" ".\Mods\Swashbuckler2e_7652d95c-1ede-6b99-e5b7-229c6a80b94f"
+mklink /J "%HOMEDRIVE%\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data\Projects\Swashbuckler2e_7652d95c-1ede-6b99-e5b7-229c6a80b94f" ".\Projects\Swashbuckler2e_7652d95c-1ede-6b99-e5b7-229c6a80b94f"
+mklink /J "%HOMEDRIVE%\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data\Public\Swashbuckler2e_7652d95c-1ede-6b99-e5b7-229c6a80b94f" ".\Public\Swashbuckler2e_7652d95c-1ede-6b99-e5b7-229c6a80b94f"
+
+pause
